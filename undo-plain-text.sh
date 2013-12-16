63707adbece704cd427153e6e65af1b5dcf76d7c
ken <ken@basistech.com>
2013-12-15 18:31:23 -0800

    Starter plain text collection example.

---
+++ undo-plain-text.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+# This script deletes single-shard collection in Solr
+
+set -e
+
+DATASET=plain-text
+
+if [ -e /var/lib/cloudera-demovm/${DATASET}-create-collection.done ]; then
+    solrctl collection --delete $DATASET
+    sudo rm /var/lib/cloudera-demovm/${DATASET}-create-collection.done
+fi
+
+if [ -e /var/lib/cloudera-demovm/${DATASET}-create-dir.done ]; then
+    solrctl instancedir --delete $DATASET
+    sudo rm /var/lib/cloudera-demovm/${DATASET}-create-dir.done
+fi
+
+if [ -e /var/lib/cloudera-demovm/${DATASET}-generate.done ]; then
+    rm -rf /home/cloudera/${DATASET}_configs
+    sudo rm /var/lib/cloudera-demovm/${DATASET}-generate.done
+fi
